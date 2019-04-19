2bd93b9acc810eabfdf4599ff7630477788475e0
Nicholas Rampton <nick.rampton@gmail.com>
2019-04-18 22:16:14 -0400

    Initial push to remote

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number from 1 to 900!");
+
+    let secret_number = rand::thread_rng().gen_range(1,901);
+
+    loop {
+
+        let fake_number = rand::thread_rng().gen_range(1,901);
+        println!("Please input your guess.");
+        println!("(You should guess {}!)", fake_number);
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
